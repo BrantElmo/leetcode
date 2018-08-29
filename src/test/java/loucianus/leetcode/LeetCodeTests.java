0660d7c20ccd8b5a90a8ca8948696efa4a6234a2
Loucianus <brantelmolee@163.com>
2018-08-29 17:54:53 +0800

    test

---
+++ src/test/java/loucianus/leetcode/LeetCodeTests.java
@@ -0,0 +1,16 @@
+package loucianus.leetcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeetCodeTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
